9c98df8730d00153e77870bd6e544834b1e7d193
Christopher Collins <pdq278@mocs.utc.edu>
2022-03-27 15:53:04 -0400

    Most recent changes.

--- HippityHoppity.py
+++ HippityHoppity.py
@@ -1,11 +1,15 @@
 ## Regular Imports ##
+import pynput
 from PIL import Image, ImageFilter, ImageGrab
 import cv2
 import numpy as np
 import time
 import mss
-from pynput.keyboard import Key, Controller
+from pynput.keyboard import Key, Listener, KeyCode, Controller
+import keyboard
 import random
+import matplotlib.pyplot as plt
+from msvcrt import getch
 from collections import namedtuple, deque
 from itertools import count
 ## Custom Imports ##
@@ -27,8 +31,6 @@ prev = 0
 
 ## Our agent's variables ##
 currentTime = time.time() # Set the time to the current time. This is to be used for the time reward.
-global jumps
-jumps = 0 # This is the number of jumps we have made.
 ## Our agent's variables ##
 
 
@@ -37,14 +39,15 @@ jumps = 0 # This is the number of jumps we have made.
 # The monitor dimensions need to be set up such that it is centered on the location of the screen where the replay button will be when you died.
 def checkIfDead():
     with mss.mss() as sct:
-        monitor = {"top": 850, "left": 620, "width": 100, "height": 100} # This is the location of the replay button.
+        monitor = {"top": 685, "left": 800, "width": 50, "height": 50} # This is the location of the replay button.
         img = np.array(sct.grab(monitor)) # Grab the pixels of the screen.
     img = Image.fromarray(img) # Convert the array to an image.
     img = img.convert("L") # Convert to grayscale
     img = img.filter(ImageFilter.Kernel((3, 3), (-1, -1, -1, -1, 8, -1, -1, -1, -1), 1, 0)) # Edge detection
     # Sum the white pixels in the image.
     whitePixels = np.sum(img) # Sum the white pixels in the image.
-    if (abs(whitePixels - 287059)/287059) < 0.005: # From a test, I found that 287059 is the average of the white pixels in the image so
+    #print(whitePixels)
+    if (abs(whitePixels - 161983)/161983) < 0.005: # From a test, I found that 287059 is the average of the white pixels in the image so
         amDead = True
         return True # Return true since we are on the replay button (death screen).
     else:
@@ -68,14 +71,14 @@ def grabScreen():
     time_elapsed = time.time() - prev
     img = None
     with mss.mss() as sct:
-        monitor = {"top": 30, "left": 270, "width": 1370, "height": 1050}
+        monitor = {"top": 300, "left": 885, "width": 395, "height": 480}
         img = np.array(sct.grab(monitor))
     img = Image.fromarray(img)
-    img = img.resize((1000, 1000))
+    img = img.resize((300, 300))
     # Remove the alpha channel.
     # Pixelate the image
     # img = img.resize((int(img.width/12), int(img.height/12)), resample=Image.BILINEAR).resize(SCREEN_SIZE,Image.NEAREST)
-    img = img.filter(ImageFilter.Kernel((3, 3), (-1, -1, -1, -1, 7, -1, -1, -1, -1), 1, 0))  # Edge detection
+    #img = img.filter(ImageFilter.Kernel((3, 3), (-1, -1, -1, -1, 7 , -1, -1, -1, -1), 1, 0))  # Edge detection
     img = img.convert("L")  # Convert to grayscale
     ## Grab the screen. ##
     return img
@@ -107,13 +110,14 @@ def imgToTensor(img):
 
 def doAction(actionIndex):
     print("Action Index: " + str(actionIndex))
-    j = jumps
     if actionIndex[0] == 1:
         pressUp()
-        j += 1
+        model.jumps += 1
+        model.timeSinceLastJump = time.time() - model.jumpTime
+        model.jumpTime = time.time()
         return 1
     else:
-        print("Doing nothing")
+        #print("Doing nothing")
         return 0
 
 
@@ -125,11 +129,12 @@ def getTimeReward(startTime):
 
 
 def frameToState(action, currentTime):
+    model.jumps += 1
     doAction(action)
     reward = 0
     terminal = False
     # We wait for a frame to pass before we grab the screen.
-    img = np.array(grabScreen()).reshape(1000, 1000, 1)
+    img = np.array(grabScreen()).reshape(300, 300, 1) # 640 x 480
     img = imgToTensor(img)
 
     terminal = checkAndReset(currentTime)
@@ -137,7 +142,12 @@ def frameToState(action, currentTime):
         currentTime    = time.time()
         reward = -10
     else:
-        reward = getTimeReward(currentTime)*10 + jumps
+        reward = \
+            (1+getTimeReward(1+currentTime))*(1+model.timeSinceLastJump)**2
+            #model.jumps # This reward might not be worth including.
+        #print("TIME REWARD: " + str(reward))
+        #print("JUMPS: " + str(model.jumps))
+        #print("REWARD: " + str(reward))
     return img, reward, terminal
 
 
@@ -153,23 +163,26 @@ class NeuralNetwork(nn.Module):
         self.final_epsilon = 0.0001 # Final value of epsilon.
         self.initial_epsilon = 0.1 # This determines how often we will explore.
         self.number_of_iterations = 2000000 # Number of times we will train the network.
-        self.replay_memory_size = 10000 # Number of experiences we will store in the replay memory.
-        self.minibatch_size = 10 # Number of experiences we will sample from the replay memory.
+        self.replay_memory_size = 5000 # Number of experiences we will store in the replay memory.
+        self.minibatch_size = 32 # Number of experiences we will sample from the replay memory.
 
-
+        # Other stuff.
+        self.jumps = 0
+        self.timeSinceLastJump = 0
+        self.jumpTime = time.time()
 
 
         self.conv1 = nn.Conv2d(4, 32, 4, 1) # Input channels, output channels, kernel size, stride
         self.relu1 = nn.ReLU(inplace=True) # Inplace is true so we don't create a new tensor.
-        self.conv2 = nn.Conv2d(32, 64, 6, 2 )
+        self.conv2 = nn.Conv2d(32, 16, 6, 2 )
         self.relu2 = nn.ReLU(inplace=True)
-        self.conv3 = nn.Conv2d(64, 64, 8,  8)
+        self.conv3 = nn.Conv2d(16, 14, 8,  8)
         self.relu3 = nn.ReLU(inplace=True)
-        self.conv4 = nn.Conv2d(64, 64, 10,  16)
+        self.conv4 = nn.Conv2d(14, 64, 10,  16)
         self.relu4 = nn.ReLU(inplace=True)
-        self.fc4 = nn.Linear(1024, 2024)
+        self.fc4 = nn.Linear(64, 1024)
         self.relu5 = nn.ReLU(inplace=True)
-        self.fc5 = nn.Linear(2024, self.number_of_actions)
+        self.fc5 = nn.Linear(1024, self.number_of_actions)
 
     def forward(self, x):
         out = self.conv1(x)
@@ -188,13 +201,14 @@ class NeuralNetwork(nn.Module):
         return out
 
 model = NeuralNetwork()
-optimizer = optim.Adam(model.parameters(), lr=1e-6)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 criterion = nn.MSELoss()
 
 replay_memory = []
 
 action = torch.zeros([model.number_of_actions], dtype=torch.float32)
-action[0] = 1
+action_index = 0
+action[0] = 0
 imgData, reward, terminal = frameToState(action, currentTime)
 state = torch.cat((imgData, imgData, imgData, imgData)).unsqueeze(0)
 
@@ -206,11 +220,12 @@ epsilon_decrements = np.linspace(model.initial_epsilon, model.final_epsilon, mod
 
 epsilon = model.initial_epsilon
 
-
 start = time.time()
+rewards_list = []
+
 
 while iteration < model.number_of_iterations:
-    print("Terminal Status At Start: " + str(terminal))
+    #print("Terminal Status At Start: " + str(terminal))
     # get output from the neural network
     output = model(state)[0]
 
@@ -221,16 +236,32 @@ while iteration < model.number_of_iterations:
 
     # epsilon greedy exploration
     random_action = random.random() <= epsilon
+
     if random_action:
         print("Performed random action!")
     action_index = [torch.randint(model.number_of_actions, torch.Size([]), dtype=torch.int)
                     if random_action
                     else torch.argmax(output)][0]
 
+    action[action_index] = 1
+
+    print("ACTION INDEX: " + str(action_index))
+    if (keyboard.is_pressed('w')):
+        print("Forced jump")
+        action = torch.zeros([model.number_of_actions], dtype=torch.float32)
+        action[action_index] = torch.tensor(1)
+    elif (keyboard.is_pressed('s')):
+        print("Forced down")
+        action = torch.zeros([model.number_of_actions], dtype=torch.float32)
+        action[action_index] = 0
+
+
+
     if torch.cuda.is_available():  # put on GPU if CUDA is available
         action_index = action_index.cuda()
 
-    action[action_index] = 1
+
+
 
 
     # get next state and reward
@@ -239,14 +270,16 @@ while iteration < model.number_of_iterations:
 
     action = action.unsqueeze(0)
     reward = torch.from_numpy(np.array([reward], dtype=np.float32)).unsqueeze(0)
-
+    rewards_list.append(reward)
 
     # save transition to replay memory
     replay_memory.append((state, action, reward, state_1, terminal))
 
     # if replay memory is full, remove the oldest transition
     if len(replay_memory) > model.replay_memory_size:
-        replay_memory.pop(0)
+        tempVal = len(replay_memory)
+        while len(replay_memory) > tempVal*0.85:
+            replay_memory.pop(0)
 
         # epsilon annealing
         epsilon = epsilon_decrements[iteration]
@@ -296,7 +329,9 @@ while iteration < model.number_of_iterations:
 
         #print("Old Current Time: " + str(currentTime))
         currentTime = time.time() # reset time
-        jumps = 0 # reset jumps
+        model.jumps = 0 # reset jumps
+        model.jumpTime = time.time() # reset jump time
+        model.timeSinceLastJump = 0 # reset time since last jump
         #print("New Current Time: " + str(currentTime))
 
 
@@ -305,13 +340,19 @@ while iteration < model.number_of_iterations:
     state = state_1
     iteration += 1
 
-    if iteration % 25000 == 0:
+    if iteration % 10000 == 0:
         torch.save(model, "current_model_" + str(iteration) + ".pth")
+
+    if iteration % 3000 == 0:
+        # Plot the reward over  time.
+        plt.plot(rewards_list)
+        plt.ylabel('Reward')
+        plt.xlabel('Episode')
+        plt.savefig("rewards_" + str(iteration) + ".png")
 
     print("iteration:", iteration, "elapsed time:", time.time() - start, "epsilon:", epsilon, "action:",
           action_index.cpu().detach().numpy(), "reward:", reward.numpy()[0][0], "Q max:",
           np.max(output.cpu().detach().numpy()))
-
 
 
 
