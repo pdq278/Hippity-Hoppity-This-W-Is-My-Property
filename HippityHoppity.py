ebca895b4d16c906e914715c0b39b235f4a721f4
Christopher Collins <pdq278@mocs.utc.edu>
2022-03-30 03:07:06 -0400

    Most recent changes.

--- HippityHoppity.py
+++ HippityHoppity.py
@@ -71,15 +71,17 @@ def grabScreen():
     time_elapsed = time.time() - prev
     img = None
     with mss.mss() as sct:
-        monitor = {"top": 300, "left": 885, "width": 395, "height": 480}
+        monitor = {"top": 300, "left": 845, "width": 435, "height": 480}
         img = np.array(sct.grab(monitor))
     img = Image.fromarray(img)
-    img = img.resize((300, 300))
+    img = img.resize((84, 84))
     # Remove the alpha channel.
     # Pixelate the image
     # img = img.resize((int(img.width/12), int(img.height/12)), resample=Image.BILINEAR).resize(SCREEN_SIZE,Image.NEAREST)
-    #img = img.filter(ImageFilter.Kernel((3, 3), (-1, -1, -1, -1, 7 , -1, -1, -1, -1), 1, 0))  # Edge detection
-    img = img.convert("L")  # Convert to grayscale
+    # img = img.filter(ImageFilter.Kernel((3, 3), (-1, -1, -1, -1, 7 , -1, -1, -1, -1), 1, 0))  # Edge detection
+    img = img.convert('L')  # Convert to grayscale
+    #img = img.convert('1')  # Convert to black and white
+    img = img.point(lambda p: 0 if p < 125  else 255, '1')  # Convert to black and white
     ## Grab the screen. ##
     return img
 
@@ -95,7 +97,7 @@ def checkAndReset(currentTime):
 
 # Shows the image in a window. If you place this in a while loop with grabScreen() it will repeatedly update the screen.
 def showInWindow(img):
-    frame = np.array(img)
+    frame = np.array(img, dtype=np.float32)
     cv2.imshow('frame', frame)
     cv2.waitKey(1)
 
@@ -134,17 +136,39 @@ def frameToState(action, currentTime):
     reward = 0
     terminal = False
     # We wait for a frame to pass before we grab the screen.
-    img = np.array(grabScreen()).reshape(300, 300, 1) # 640 x 480
+    img = np.array(grabScreen()).reshape(84, 84, 1) # 640 x 480
     img = imgToTensor(img)
 
     terminal = checkAndReset(currentTime)
+    timeReward = getTimeReward(currentTime)
     if (terminal):
-        currentTime    = time.time()
-        reward = -10
+        currentTime = time.time()
+        reward -= 10
+        reward -= model.jumps
+
+        model.timeList.append(timeReward)
+        avgTime = np.mean(model.timeList)
+        if (timeReward > avgTime):
+            print("Beat the average time of " + str(avgTime))
+            reward += 20
+        else:
+            print("Did not beat the average time of " + str(avgTime))
+            reward -= 20
     else:
-        reward = \
-            (1+getTimeReward(1+currentTime))*(1+model.timeSinceLastJump)**2
-            #model.jumps # This reward might not be worth including.
+
+
+        bestTimeBonus = 0
+        if (timeReward > model.bestTime):
+            model.bestTime = timeReward
+            bestTimeBonus = model.bestTime*100
+
+        if (model.jumps == 0):
+            reward = -50
+
+
+        #reward = (1+timeReward)**2 + (1+model.timeSinceLastJump)**2 + bestTimeBonus
+        reward += bestTimeBonus
+        #model.jumps # This reward might not be worth including.
         #print("TIME REWARD: " + str(reward))
         #print("JUMPS: " + str(model.jumps))
         #print("REWARD: " + str(reward))
@@ -163,44 +187,61 @@ class NeuralNetwork(nn.Module):
         self.final_epsilon = 0.0001 # Final value of epsilon.
         self.initial_epsilon = 0.1 # This determines how often we will explore.
         self.number_of_iterations = 2000000 # Number of times we will train the network.
-        self.replay_memory_size = 5000 # Number of experiences we will store in the replay memory.
-        self.minibatch_size = 32 # Number of experiences we will sample from the replay memory.
+        self.replay_memory_size = 10000 # Number of experiences we will store in the replay memory.
+        self.minibatch_size = 64 # Number of experiences we will sample from the replay memory.
 
         # Other stuff.
         self.jumps = 0
         self.timeSinceLastJump = 0
         self.jumpTime = time.time()
+        self.bestTime = 0
+        self.timeList = []
+        self.imgTensors = []
 
 
-        self.conv1 = nn.Conv2d(4, 32, 4, 1) # Input channels, output channels, kernel size, stride
+        self.conv1 = nn.Conv2d(4, 32, 16, 4) # Input channels, output channels, kernel size, stride
         self.relu1 = nn.ReLU(inplace=True) # Inplace is true so we don't create a new tensor.
         self.conv2 = nn.Conv2d(32, 16, 6, 2 )
         self.relu2 = nn.ReLU(inplace=True)
-        self.conv3 = nn.Conv2d(16, 14, 8,  8)
-        self.relu3 = nn.ReLU(inplace=True)
-        self.conv4 = nn.Conv2d(14, 64, 10,  16)
-        self.relu4 = nn.ReLU(inplace=True)
-        self.fc4 = nn.Linear(64, 1024)
-        self.relu5 = nn.ReLU(inplace=True)
-        self.fc5 = nn.Linear(1024, self.number_of_actions)
+        self.fc4 = nn.Linear(784, 1024)
+        self.leakyrelu1 = nn.LeakyReLU(0.1, inplace=True)
+        self.fc5 = nn.Linear(1024, 1024)
+        self.leakyrelu2 = nn.LeakyReLU(0.1, inplace=True)
+        self.fc6 = nn.Linear(1024, self.number_of_actions)
+
+    def init_weights(self, m):
+        if (type(m) == nn.Conv2d or type(m) == nn.Linear):
+            torch.nn.init.uniform(m.weight, -0.01, 0.01)
+            m.bias.data.fill_(0.01)
+
+    # We want to have an imgTensor list of size 4. If the list is already of size 4, we remove the first element and add the new element to the end.
+    def addStateImage(self, imgTensor):
+        if (len(self.imgTensors) >= 4):
+            self.imgTensors.pop(0)
+        else:
+            while (len(self.imgTensors) < 4):
+                self.imgTensors.append(imgTensor)
+
+
 
     def forward(self, x):
         out = self.conv1(x)
         out = self.relu1(out)
         out = self.conv2(out)
         out = self.relu2(out)
-        out = self.conv3(out)
-        out = self.relu3(out)
-        out = self.conv4(out)
-        out = self.relu4(out)
         out = out.view(out.size(0), -1)
         out = self.fc4(out)
-        out = self.relu5(out)
+        out = self.leakyrelu1(out)
         out = self.fc5(out)
+        out = self.leakyrelu2(out)
+        out = self.fc6(out)
 
         return out
 
 model = NeuralNetwork()
+model.apply(model.init_weights)
+# Load model "current_model_310000.pth"
+#model = torch.load("current_model_310000.pth", map_location='cpu' if not torch.cuda.is_available() else None)
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 criterion = nn.MSELoss()
 
@@ -210,7 +251,9 @@ action = torch.zeros([model.number_of_actions], dtype=torch.float32)
 action_index = 0
 action[0] = 0
 imgData, reward, terminal = frameToState(action, currentTime)
-state = torch.cat((imgData, imgData, imgData, imgData)).unsqueeze(0)
+model.addStateImage(imgData)
+
+state = torch.cat((model.imgTensors[0], model.imgTensors[1], model.imgTensors[2], imgData)).unsqueeze(0)
 
 epsilon = model.initial_epsilon
 iteration = 0
@@ -245,7 +288,7 @@ while iteration < model.number_of_iterations:
 
     action[action_index] = 1
 
-    print("ACTION INDEX: " + str(action_index))
+
     if (keyboard.is_pressed('w')):
         print("Forced jump")
         action = torch.zeros([model.number_of_actions], dtype=torch.float32)
@@ -281,47 +324,47 @@ while iteration < model.number_of_iterations:
         while len(replay_memory) > tempVal*0.85:
             replay_memory.pop(0)
 
-        # epsilon annealing
-        epsilon = epsilon_decrements[iteration]
+    # epsilon annealing
+    epsilon = epsilon_decrements[iteration]
 
-        # sample random minibatch
-        minibatch = random.sample(replay_memory, min(len(replay_memory), model.minibatch_size))
+    # sample random minibatch
+    minibatch = random.sample(replay_memory, min(len(replay_memory), model.minibatch_size))
 
-        # unpack minibatch
-        state_batch = torch.cat(tuple(d[0] for d in minibatch))
-        action_batch = torch.cat(tuple(d[1] for d in minibatch))
-        reward_batch = torch.cat(tuple(d[2] for d in minibatch))
-        state_1_batch = torch.cat(tuple(d[3] for d in minibatch))
+    # unpack minibatch
+    state_batch = torch.cat(tuple(d[0] for d in minibatch))
+    action_batch = torch.cat(tuple(d[1] for d in minibatch))
+    reward_batch = torch.cat(tuple(d[2] for d in minibatch))
+    state_1_batch = torch.cat(tuple(d[3] for d in minibatch))
 
-        if torch.cuda.is_available():  # put on GPU if CUDA is available
-            state_batch = state_batch.cuda()
-            action_batch = action_batch.cuda()
-            reward_batch = reward_batch.cuda()
-            state_1_batch = state_1_batch.cuda()
+    if torch.cuda.is_available():  # put on GPU if CUDA is available
+        state_batch = state_batch.cuda()
+        action_batch = action_batch.cuda()
+        reward_batch = reward_batch.cuda()
+        state_1_batch = state_1_batch.cuda()
 
         # get output for the next state
-        output_1_batch = model(state_1_batch)
+    output_1_batch = model(state_1_batch)
 
         # set y_j to r_j for terminal state, otherwise to r_j + gamma*max(Q)
-        y_batch = torch.cat(tuple(reward_batch[i] if minibatch[i][4]
+    y_batch = torch.cat(tuple(reward_batch[i] if minibatch[i][4]
                                   else reward_batch[i] + model.gamma * torch.max(output_1_batch[i])
                                   for i in range(len(minibatch))))
 
         # extract Q-value
-        q_value = torch.sum(model(state_batch) * action_batch, dim=1)
+    q_value = torch.sum(model(state_batch) * action_batch, dim=1)
 
-        # PyTorch accumulates gradients by default, so they need to be reset in each pass
-        optimizer.zero_grad()
+    # PyTorch accumulates gradients by default, so they need to be reset in each pass
+    optimizer.zero_grad()
 
-        # returns a new Tensor, detached from the current graph, the result will never require gradient
-        y_batch = y_batch.detach()
+    # returns a new Tensor, detached from the current graph, the result will never require gradient
+    y_batch = y_batch.detach()
 
-        # calculate loss
-        loss = criterion(q_value, y_batch)
+    # calculate loss
+    loss = criterion(q_value, y_batch)
 
-        # do backward pass
-        loss.backward()
-        optimizer.step()
+    # do backward pass
+    loss.backward()
+    optimizer.step()
 
     #print("TERMINAL VALUE: ", terminal)
     if (terminal):
@@ -353,9 +396,4 @@ while iteration < model.number_of_iterations:
     print("iteration:", iteration, "elapsed time:", time.time() - start, "epsilon:", epsilon, "action:",
           action_index.cpu().detach().numpy(), "reward:", reward.numpy()[0][0], "Q max:",
           np.max(output.cpu().detach().numpy()))
-
-
-
-
-
 
